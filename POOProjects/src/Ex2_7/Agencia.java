cf63556385120d936d92e24036dd74b81f21cb8a
José Otávio Ávila Alves Pires <jose.otavio2707@gmail.com>
2025-03-03 20:40:04 -0300

    Resolução do exercício 2_7

--- POOProjects/src/Ex2_7/Agencia.java
+++ POOProjects/src/Ex2_7/Agencia.java
@@ -1,4 +1,46 @@
 package Ex2_7;
 
 public class Agencia {
+    private static int num;
+    private String city;
+    private int opYear;
+
+    public Agencia(String city, int opYear) {
+        setCity(city);
+        setOpYear(opYear);
+        num++;
+    }
+
+    public int getNum() {
+        return this.num;
+    }
+
+    public int getOpYear() {
+        return this.opYear;
+    }
+
+    private boolean setOpYear(int opYear) {
+        if(opYear > 0 && opYear <= 9999){
+            this.opYear = opYear;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    private boolean setCity(String city) {
+        if(!city.isBlank()){
+            this.city = city;
+            return true;
+        } else
+            return false;
+    }
+
+    public String toString() {
+        return "Agencia [número = " + this.getNum() + ", cidade = " + this.getCity() + ", ano de abertura = " + this.getOpYear() + "]";
+    }
 }
--- POOProjects/src/Ex2_7/Banco.java
+++ POOProjects/src/Ex2_7/Banco.java
@@ -1,4 +1,70 @@
 package Ex2_7;
 
+import java.util.ArrayList;
+
 public class Banco {
+    private String nome;
+    private int cod;
+    private ArrayList<Agencia> agencias;
+
+    public Banco(String nome) {
+        setNome(nome);
+        this.agencias = new ArrayList<Agencia>();
+    }
+
+    public boolean setNome(String nome) {
+        if(!nome.isBlank()){
+            this.nome = nome;
+            return true;
+        } else
+            return false;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public ArrayList<Agencia> getAgencias() {
+        return this.agencias;
+    }
+
+    public void addAgencia(Agencia agencia) {
+        this.agencias.add(agencia);
+    }
+
+    public void removeAgencia(Agencia agencia) {
+        this.agencias.remove(agencia);
+    }
+
+    public ArrayList<Agencia> getAgencias(int ano) {
+        ArrayList <Agencia> ag = new ArrayList<Agencia>();
+        for (Agencia agencia : this.agencias) {
+            if(agencia.getOpYear() == ano){
+                ag.add(agencia);
+            }
+        }
+        return ag;
+    }
+
+    public ArrayList<Agencia> getAgencias(String city) {
+        ArrayList<Agencia> ag = new ArrayList<Agencia>();
+        for (Agencia agencia : this.agencias) {
+            if(agencia.getCity().equals(city)){
+                ag.add(agencia);
+            }
+        }
+        return ag;
+    }
+
+    public String ImprimeAgencias() {
+        String texto = "";
+        for(Agencia ag : this.agencias){
+            texto += "Agência " + ag + ag.toString();
+        }
+        return "Banco: " + this.getNome() + "\n" + texto;
+    }
 }
